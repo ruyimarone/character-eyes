135c05f7ab9bed1f6ae359b5edc411e11f0ef18b
Marc Marone <ruyi.marone@gmail.com>
2019-03-20 23:15:02 -0700

    tweak asym docs

--- AsymBiLSTM.py
+++ AsymBiLSTM.py
@@ -1,19 +1,19 @@
 import dynet as dy
 
-
-#stolen from dynet source (python/_dynet.pyx)
 class AsymBiRNNBuilder(object):
     """
-    Builder for BiRNNs that delegates to regular RNNs and wires them together.
 
-        builder = BiRNNBuilder(1, 128, 100, model, LSTMBuilder)
-        [o1,o2,o3] = builder.transduce([i1,i2,i3])
+    (MODIFIED/STOLEN from the dynet source (python/_dynet.pyx))
+
+    Modified to wire differently dimensioned (asymmetric) RNNs
+    Can also handle degenerate asymmetric RNNs, where either the forward or backward dimension is 0.
+    Provides a convenience method to get the final hidden states of sub rnn.
     """
     def __init__(self, num_layers, input_dim, hidden_dim, model, rnn_builder_factory, builder_layers=None, logger = None):
         """Args:
             num_layers: depth of the BiRNN
             input_dim: size of the inputs
-            hidden_dim: size of the outputs (and intermediate layer representations.) This hidden dim is split evenly between the two constituent RNNs, and thus must be even.
+            hidden_dim: size of the outputs (and intermediate layer representations.) This can be a tuple, specifiying different sizes for the fwd and bwd RNNs.
             model
             rnn_builder_factory: RNNBuilder subclass, e.g. LSTMBuilder
             builder_layers: list of (forward, backward) pairs of RNNBuilder instances to directly initialize layers
@@ -76,31 +76,10 @@ class AsymBiRNNBuilder(object):
 
     def add_inputs(self, es):
         """
-        returns the list of state pairs (stateF, stateB) obtained by adding
-        inputs to both forward (stateF) and backward (stateB) RNNs.
-        Does not preserve the internal state after adding the inputs.
-        Args:
-            es (list): a list of Expression
-        see also transduce(xs)
-        code:`.transduce(xs)` is different from .add_inputs(xs) in the following way:
-        - code:`.add_inputs(xs)` returns a list of RNNState pairs. RNNState objects can be
-             queried in various ways. In particular, they allow access to the previous
-             state, as well as to the state-vectors (h() and s() )
-        - :code:`.transduce(xs)` returns a list of Expression. These are just the output
-             expressions. For many cases, this suffices.
-             transduce is much more memory efficient than add_inputs.
+        Not implemented for asymmetric RNNs
         """
-        # for e in es:
-            # ensure_freshness(e)
-        raise Exception("this method should never be used!")
-        for (fb,bb) in self.builder_layers[:-1]:
-            fs = fb.initial_state().transduce(es)
-            bs = bb.initial_state().transduce(reversed(es))
-            es = [dy.concatenate([f,b]) for f,b in zip(fs, reversed(bs))]
-        (fb,bb) = self.builder_layers[-1]
-        fs = fb.initial_state().add_inputs(es)
-        bs = bb.initial_state().add_inputs(reversed(es))
-        return [(f,b) for f,b in zip(fs, reversed(bs))]
+
+        raise NotImplementedError("Not implemented for asymmetric RNNs")
 
     def transduce(self, es):
         """
@@ -131,7 +110,8 @@ class AsymBiRNNBuilder(object):
 
     def final_hiddens(self, es):
         """
-            like transduce, but it returns the last hiddens
+        Like transduce, but it returns the final hidden state of each sub RNN.
+        This is NOT the hidden state of the last character.
         """
         for (fb,bb) in self.builder_layers:
             fs = fb.initial_state().transduce(es) if self.forward_dim != 0 else None
